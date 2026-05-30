66343d1808dc43dcec90cdfe7f2425b8c00e760e
Nishitha Vemula <140103573+Nishitha-Vemula-30@users.noreply.github.com>
2026-05-30 23:52:33 +0530

    Update cookie settings for secure logout

--- Backend/Controllers/authControllers.js
+++ Backend/Controllers/authControllers.js
@@ -118,9 +118,9 @@ export const login = async (req, res) => {
 
       httpOnly: true,
 
-      secure: false,
+      secure: true,
 
-      sameSite: "lax",
+      sameSite: "none",
 
       maxAge: 7 * 24 * 60 * 60 * 1000
 
@@ -158,32 +158,21 @@ export const login = async (req, res) => {
 
 // LOGOUT
 export const logout = async (req, res) => {
-
   try {
-
-    res.cookie("token", "", {
-
+    res.clearCookie("token", {
       httpOnly: true,
-      expires: new Date(0)
+      secure: true,
+      sameSite: "none",
+    });
 
-    })
-
-    res.status(200).json({
-
+    return res.status(200).json({
       success: true,
-      message: "Logout Successful"
-
-    })
-
+      message: "Logout Successful",
+    });
   } catch (error) {
-
-    res.status(500).json({
-
+    return res.status(500).json({
       success: false,
-      message: error.message
-
-    })
-
+      message: error.message,
+    });
   }
-
-}
+};
